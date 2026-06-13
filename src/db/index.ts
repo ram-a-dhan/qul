c94917e58e4d144d9cf27ef8e931d614fbdfe59d
ram-a-dhan <ram.adhan@outlook.com>
2026-06-13 19:00:23 +0700

    perf: fingerprint table composite primary key

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,3 +8,15 @@ config({ path: ".env" });
 const sql = neon(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql, { schema });
+
+// // Local postgres DB testing mode
+// import { config } from "dotenv";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { Pool } from "pg";
+// import * as schema from "./schema";
+
+// config({ path: ".env" });
+
+// const client = new Pool({ connectionString: process.env.DATABASE_URL });
+
+// export const db = drizzle({ client, schema });
